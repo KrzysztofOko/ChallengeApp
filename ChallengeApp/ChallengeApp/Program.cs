dbea57241ca721f123fd57329ccb5027e726559c
Krzysztof Oczkowski <krzysztof.oczkowski@op.pl>
2023-08-30 16:58:15 +0200

    praca domowa do dnia 4

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,2 +1,27 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿
+using System.ComponentModel.Design;
+var age = 29;
+
+
+var age = 17;
+
+
+
+if (gender == "mezczyzna" && age < 18)
+{
+   Console.WriteLine("Niepelnoletni mezczyzna");
+}
+
+var age1 = 29;
+var gender1 = "kobieta";
+var name = "Ewa";
+
+if (name == "Ewa" && age1 == 29 + 4);
+{
+    Console.WriteLine("Ewa,lat 33");
+}
+if (gender1 == "kobieta" && age1 < 30);
+{
+    Console.WriteLine("Kobieta ponizej 30 lat");
+}
+
