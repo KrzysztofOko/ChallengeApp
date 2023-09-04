fa43186a577a832b9f57ad53fee37b6f097db577
Krzysztof Oczkowski <krzysztof.oczkowski@op.pl>
2023-09-04 18:23:10 +0200

    zadanie domowe do lekcji 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -1,16 +1,29 @@
-﻿var name = "Ewa";
-var gender = "kobieta";
-var age = 29;
+﻿int number = 1972;
+string numberAsString = number.ToString();
+char[] letters = numberAsString.ToCharArray();
 
-if (age < 30 && gender == "kobieta")
+int counter0  = 0;
+int counter1 = 1;
+int counter2 = 1;
+int counter3 = 0;
+int counter4 = 0;
+int counter5 = 0;
+int counter6 = 0;
+int counter7 = 1;
+int counter8 = 0;
+int counter9 = 1;
+
+foreach (char letter in letters)
 {
-    Console.WriteLine("kobieta poniżej 30 lat");
+    if (letter == '0') {counter0++;}
+    else if (letter == '1') {counter1++;}
+    else if (letter == '2') {counter2++;}
+    else if (letter == '3') {counter3++;}
+    else if (letter == '4') {counter4++;}
+    else if (letter == '5') {counter5++;}
+    else if (letter == '6') {counter6++;}
+    else if (letter == '7') {counter7++;}
+    else if (letter == '8') {counter8++;}
+    else if (letter == '9') { counter9++;}
 }
- else if (age == 29 + 4 && name == "Ewa")
-{
-    Console.WriteLine("Ewa, lat 33");
-}
-else if (age < 18 && gender == "meżczyzna")
-{
-    Console.WriteLine("Niepełnoletni mężczyzna");
-}
+Console.WriteLine(numberAsString);
