655df950362b4455bf18c33902b303ae6aa3a9d6
Krzysztof Oczkowski <krzysztof.oczkowski@op.pl>
2023-09-05 16:18:23 +0200

    praca domowa do lekcji 5

--- ChallengeApp/ChallengeApp/Program.cs
+++ ChallengeApp/ChallengeApp/Program.cs
@@ -2,16 +2,16 @@
 string numberAsString = number.ToString();
 char[] letters = numberAsString.ToCharArray();
 
-int counter0  = 0;
-int counter1 = 1;
-int counter2 = 1;
+int counter0 = 0;
+int counter1 = 0;
+int counter2 = 0;
 int counter3 = 0;
 int counter4 = 0;
 int counter5 = 0;
 int counter6 = 0;
-int counter7 = 1;
+int counter7 = 0;
 int counter8 = 0;
-int counter9 = 1;
+int counter9 = 0;
 
 foreach (char letter in letters)
 {
@@ -26,4 +26,13 @@ foreach (char letter in letters)
     else if (letter == '8') {counter8++;}
     else if (letter == '9') { counter9++;}
 }
-Console.WriteLine(numberAsString);
+Console.WriteLine("0 =>" + counter0);
+Console.WriteLine("1 =>" + counter1);
+Console.WriteLine("2 =>" + counter2);
+Console.WriteLine("3 =>" + counter3);
+Console.WriteLine("4 =>" + counter4);
+Console.WriteLine("5 =>" + counter5);
+Console.WriteLine("6 =>" + counter6);
+Console.WriteLine("7 =>" + counter7);
+Console.WriteLine("8 =>" + counter8);
+Console.WriteLine("9 =>" + counter9);
